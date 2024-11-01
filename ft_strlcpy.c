0effe307bd88ea32e346c765b99abd9a16d72f90
kernelpanic <abdertouaitia@gmail.com>
2024-11-01 01:15:16 +0100

    l

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,31 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abtouait <abtouait@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/22 02:31:06 by abtouait          #+#    #+#             */
-/*   Updated: 2024/11/01 01:12:55 by abtouait         ###   ########.fr       */
+/*   Created: 2024/10/31 20:47:17 by abtouait          #+#    #+#             */
+/*   Updated: 2024/11/01 01:12:36 by abtouait         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <unistd.h>
-# include <stdio.h>
-# include <stddef.h>
+size_t	ft_strlcpy(char *dest, const char *src, size_t size )
+{
+	size_t	i;
 
-int	ft_isdigit(int c);
-size_t	ft_strlen(const char *s);
-int	ft_atoi(const char *str);
-int	ft_isalpha(int c);
-int	ft_tolower(int c);
-int	ft_toupper(int c);
-int ft_isalnum(int c);
-int ft_isascii(int c);
-int ft_isprint(int c);
-size_t	ft_strlcpy(char *dest, const char *src, size_t size );
-
-#endif
+	i = 0;
+	if (src[i] == '\0')
+	{
+		dest[i] = '\0';
+		return (ft_strlen(src));
+	}
+	if (size == 0)
+	{
+		return (ft_strlen(src));
+	}
+	while (src[i] != '\0' && size > i + 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (ft_strlen(src));
+}
